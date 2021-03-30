04f36f399e4ca9ff4ccdef77096d68117076962e
tif-calin <culitif@tuta.io>
2021-03-30 15:17:57 -0700

    set up initializer function

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // import functions and grab DOM elements
+import { createListing } from './utils.js';
+import { products } from './products';
+
+const store = document.querySelector('#products-listing');
 
 // initialize state
+const loadItems = (listings) => {
+    for (let listing of listings) {
+        store.appendChild(createListing(listing));
+    }
+};
 
 // set event listeners to update state and DOM
+loadItems(products);
--- utils.js
+++ utils.js
@@ -1,4 +1,7 @@
 
 export const createListing = obj => {
     // create an <li> and return it
+    const listItem = document.createElement('li');
+
+    return listItem;
 };
