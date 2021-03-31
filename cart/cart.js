7fbdd463a0a6d367cd13252745b939ea77d9ff2d
tif-calin <culitif@tuta.io>
2021-03-31 14:54:39 -0700

    create cart.js

---
+++ cart/cart.js
@@ -0,0 +1,14 @@
+export const cart = [
+    {
+        'id': '0boq',
+        'quantity': 0
+    },
+    {
+        'id': '1syn',
+        'quantity': 3
+    },
+    {
+        'id': '5sau',
+        'quantity': 1
+    }
+];
