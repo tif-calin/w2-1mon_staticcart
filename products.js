812e1f9b719681534b92433769dd4aa926decc73
tif-calin <culitif@tuta.io>
2021-03-31 08:40:10 -0700

    add walking onion

--- products.js
+++ products.js
@@ -22,7 +22,20 @@ export const products = [
                 'description': "As with most 'parasitic' plants, there's much more to the dodder's relationships with its host plants than revealed by its much stigmatized label. This vine has created an alternative to the Wood Wide Web allowing plants to communicate with each other through the connections this plant makes&mdash;even between species! Plants are able to send warning signals to others in case of pests or other dangers. In addition, genetic analysis has also found it borrows a lot of its DNA from plants it parasitizes.",
             }
         ],
-        'price': 999,
+        'price': 23.50,
+        'is-tree': false
+    },
+    {
+        'name': 'Walking onion',
+        'species': 'Allium × proliferum',
+        'image': 'Allium-proliferum.jpg',
+        'superpowers': [
+            {
+                'name': 'Literally Walks',
+                'description': "Instead of flowers, the walking onion creates a cluster of bulbets. These bulbets eventually grow large enough that the stalks bend down under the weight, causing the bulbs to self-plant. Through this cycle, the onion is said to 'walk' across a garden. If walking your dog is a bit too high of a pace for you, try taking this botanical friend with you instead!",
+            }
+        ],
+        'price': 8.99,
         'is-tree': false
     }
 ];
