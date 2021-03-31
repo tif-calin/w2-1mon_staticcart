0e6a7e606431dbe05d912ad44ad2e2a787693aed
tif-calin <culitif@tuta.io>
2021-03-31 08:12:56 -0700

    add createListing function

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // import functions and grab DOM elements
 import { createListing } from './utils.js';
-import { products } from './products';
+import { products } from './products.js';
 
 const store = document.querySelector('#products-listing');
 
 // initialize state
 const loadItems = (listings) => {
+    store.innerHTML = '';
     for (let listing of listings) {
         store.appendChild(createListing(listing));
     }
